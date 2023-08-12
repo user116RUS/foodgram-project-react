8761a74af2fda15a4db6b19287bc4405888f8278
ramilnurgaleev <tatarian2009@gmail.com>
2023-08-12 20:23:20 +0300

    pep8

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from users.models import Subscription, User
+from recipes.serializers import SmallRecipeSerializer
 
 
 class CustomUserSerializer(serializers.ModelSerializer):
