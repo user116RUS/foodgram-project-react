dde387505dfacf6c8a75ec343989a715d350419b
ramilnurgaleev <tatarian2009@gmail.com>
2023-08-16 11:47:34 +0300

    without version

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -8,13 +8,13 @@ from users.views import CustomUserViewSet
 
 app_name = 'api'
 
-router_v1 = DefaultRouter()
-router_v1.register('users', CustomUserViewSet, basename='users')
-router_v1.register('recipes', RecipeViewSet, basename='recipes')
-router_v1.register('tags', TagViewSet, basename='tags')
-router_v1.register('ingredients', IngredientViewSet, basename='ingredients')
+router = DefaultRouter()
+router.register('users', CustomUserViewSet, basename='users')
+router.register('recipes', RecipeViewSet, basename='recipes')
+router.register('tags', TagViewSet, basename='tags')
+router.register('ingredients', IngredientViewSet, basename='ingredients')
 
 urlpatterns = (
-    path("", include(router_v1.urls)),
+    path("", include(router.urls)),
     path("auth/", include("djoser.urls.authtoken")),
 )
