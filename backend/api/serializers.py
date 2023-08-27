20dd4f5d674e1050df6ea831c39671d8273a0079
ramilnurgaleev <tatarian2009@gmail.com>
2023-08-27 17:16:59 +0300

    workflow

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -135,7 +135,7 @@ class RecipeSerializer(serializers.ModelSerializer):
     def get_is_favorited(self, obj):
         """Рецепт в избранном или нет."""
         user = self.context.get('request').user
-        return user.favorites.filter(recipe=obj.id).exists()
+        return user.favorites.filter(recipe=obj).exists()
 
     def get_is_in_shopping_cart(self, obj):
         """Рецепт в списке покупок."""
