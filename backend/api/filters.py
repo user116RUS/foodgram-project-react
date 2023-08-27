f646d5f350e8d2d1e85db7e1fedf19d3d93060ce
ramilnurgaleev <tatarian2009@gmail.com>
2023-08-27 20:04:22 +0300

    workflow

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -3,33 +3,35 @@ from django_filters.rest_framework import FilterSet, filters
 from rest_framework.filters import SearchFilter
 
 from recipes.models import Recipe, Tag
+from django_filters import rest_framework as filter
 from users.models import User
 
 
-class RecipeFilter(FilterSet):
-    tags = ModelMultipleChoiceFilter(
+class RecipeFilter(filter.FilterSet):
+    author = filter.CharFilter()
+    tags = filter.ModelMultipleChoiceFilter(
         field_name='tags__slug',
-        to_field_name='slug',
-        queryset=Tag.objects.all()
+        queryset=Tag.objects.all(),
+        label='Tags',
+        to_field_name='slug'
     )
-
-    author = filters.ModelChoiceFilter(queryset=User.objects.all())
-    is_favorited = filters.BooleanFilter(method='filter_favorited')
-    is_in_shopping_cart = filters.BooleanFilter(method='filter_shopping_cart')
-
-    def filter_favorited(self, queryset, name, value):
-        if value:
-            return queryset.filter(marked_by__user=self.request.user)
-        return queryset
-
-    def filter_shopping_cart(self, queryset, name, value):
-        if value:
-            return queryset.filter(recipe_shopping_cart__user=self.request.user)
-        return queryset
+    is_favorited = filter.BooleanFilter(method='get_favorite')
+    is_in_shopping_cart = filter.BooleanFilter(
+        method='get_is_in_shopping_cart')
 
     class Meta:
         model = Recipe
-        fields = ('tags', 'author', 'is_favorited', 'is_in_shopping_cart')
+        fields = ['tags', 'author', 'is_favorited', 'is_in_shopping_cart']
+
+    def get_favorite(self, queryset, name, value):
+        if value:
+            return queryset.filter(favorites__user=self.request.user)
+        return queryset
+
+    def get_is_in_shopping_cart(self, queryset, name, value):
+        if value:
+            return queryset.filter(shopping_cart__user=self.request.user)
+        return queryset
 
 
 class SearchingFilter(SearchFilter):
