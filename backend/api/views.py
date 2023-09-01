d4ea11ef96894b0df506aef10a5e274a53ef92c2
ramilnurgaleev <tatarian2009@gmail.com>
2023-09-01 13:32:01 +0300

    debugged

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from fpdf import FPDF
-from rest_framework import status, viewsets, permissions
+from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
